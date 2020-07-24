f0ce4dab8773fee369a227e0c152421be356bfe6
兔子先生 <1993iweb@gmail.com>
2020-07-24 18:02:20 +0800

    设置响应的回调，初步设计

--- source/src/xhr.ts
+++ source/src/xhr.ts
@@ -1,21 +1,38 @@
-import { AxiosRequestConfig } from './types/index'
+import { AxiosRequestConfig, AxiosPromise } from './types/index'
+import { resolve } from 'dns'
 
 // 创建最基本的请求发送
 
-export default function xhr(config: AxiosRequestConfig): void {
-  const { url, method = 'get', data = null, headers } = config
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise(resolve => {
+    const { url, method = 'get', data = null, headers, responseType } = config
 
-  const request = new XMLHttpRequest()
+    const request = new XMLHttpRequest()
 
-  request.open(method.toUpperCase(), url, true)
-
-  Object.keys(headers).forEach(name => {
-    if (data === null && name.toLocaleUpperCase() === 'content-type') {
-      delete headers[name]
-    } else {
-      request.setRequestHeader(name, headers[name])
+    if (responseType) {
+      // 设置响应类型
+      request.responseType = responseType
     }
-  })
 
-  request.send(data)
+    request.open(method.toUpperCase(), url, true)
+
+    request.onreadystatechange = () => {
+      if (request.readyState !== 4) {
+        return
+      }
+
+      const responseHeaders = request.getAllResponseHeaders() // 获取请求头
+      const responseData = responseType !== 'text' ? request.response : request.responseText // 根据设置的requsetType 来判断从response还是responseText获取响应数据
+    }
+
+    Object.keys(headers).forEach(name => {
+      if (data === null && name.toLocaleUpperCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
+
+    request.send(data)
+  })
 }
