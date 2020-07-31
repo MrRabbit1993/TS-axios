7e1e76d197790c66500b5786a17447d72a348476
兔子先生 <1993iweb@gmail.com>
2020-07-31 16:45:43 +0800

    feat: 拦截器实现 （添加拦截器、删除拦截器）

--- src/core/InterceptorManager.ts
+++ src/core/InterceptorManager.ts
@@ -6,24 +6,28 @@ interface Interceptor<T> {
     rejected?: RejectedFn
 }
 export default class InterceptorManager<T> {
-    private interceptors: Array<Interceptor<T>> | null  // 私有属性，存储拦截器  联合类型
+    private interceptors: Array<Interceptor<T> | null>  // 私有属性，存储拦截器  联合类型
 
     constructor() {
         this.interceptors = []
     }
-    use(resolved: ResolvedFn<T>, rejected: RejectedFn): number {
+    use(resolved: ResolvedFn<T>, rejected: RejectedFn): number { // 添加拦截器
         this.interceptors.push({
             resolved,
             rejected
         })
-        return this.interceptors.length
+        return this.interceptors.length - 1
     }
-    eject(id: number): void {
-        if (this.interceptors[id]) {
+    eject(id: number): void { // 删除拦截器
+        if (this.interceptors[id]) {// 这里做拦截器的删除，（将对应的拦截器id置为null）
             this.interceptors[id] = null
         }
     }
-    forEach(fn: (interceptor: Interceptor<T>)) {
-
+    forEach(fn: (interceptor: Interceptor<T>) => void): void {// 便于外部访问私有属性,遍历拦截器 interceptors, 参数是fn函数 fn的参数是interceptor，类型Interceptor<T>，fn返回值void
+        this.interceptors.forEach(interceptor => {// 遍历拦截器
+            if (interceptor !== null) {// 存在拦截器 执行一次
+                fn(interceptor)
+            }
+        })
     }
 }
