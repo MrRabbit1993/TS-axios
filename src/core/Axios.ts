5e79bb82726a771fd775311d0e4498e9aaf1ec3e
兔子先生 <1993iweb@gmail.com>
2020-11-14 21:33:16 +0800

    请求测试用例

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -1,113 +1,137 @@
-import { AxiosRequestConfig, AxiosPromise, Method, AxiosResponse, ResolvedFn, RejectedFn } from "./../types"
+import {
+  AxiosRequestConfig,
+  AxiosPromise,
+  Method,
+  AxiosResponse,
+  ResolvedFn,
+  RejectedFn
+} from './../types'
 
-import dispatchRequest, { transformURL } from "./dispatchRequest"
+import dispatchRequest, { transformURL } from './dispatchRequest'
 
-import InterceptorManager from "./InterceptorManager"
+import InterceptorManager from './InterceptorManager'
 
-import mergeConfig from "./mergeConfig"
-
+import mergeConfig from './mergeConfig'
 
 interface Interceptors {
-    request: InterceptorManager<AxiosRequestConfig>
-    response: InterceptorManager<AxiosResponse>
+  request: InterceptorManager<AxiosRequestConfig>
+  response: InterceptorManager<AxiosResponse>
 }
 
 interface PromiseChain<T> {
-    resolved: ResolvedFn<T> | ((config: AxiosRequestConfig) => AxiosPromise)
-    rejected?: RejectedFn
+  resolved: ResolvedFn<T> | ((config: AxiosRequestConfig) => AxiosPromise)
+  rejected?: RejectedFn
 }
 
 export default class Axios {
-    defaults: AxiosRequestConfig
-    interceptors: Interceptors
-    constructor(initConfig: AxiosRequestConfig) {
-        this.defaults = initConfig
-        this.interceptors = {
-            request: new InterceptorManager<AxiosRequestConfig>(),
-            response: new InterceptorManager<AxiosResponse>()
-        }
+  defaults: AxiosRequestConfig
+  interceptors: Interceptors
+  constructor(initConfig: AxiosRequestConfig) {
+    this.defaults = initConfig
+    this.interceptors = {
+      request: new InterceptorManager<AxiosRequestConfig>(),
+      response: new InterceptorManager<AxiosResponse>()
     }
-    // request(config: AxiosRequestConfig): AxiosPromise {
-    //     return dispatchRequest(config)
-    // }
-    // 函数重载扩展请求
-    request(url: any, config?: any): AxiosPromise {
-        if (typeof url === 'string') { // 对应 axios('url',config)
-            if (!config) config = {}
-            config.url = url
-        } else {
-            config = url // 对应 axios(config)
-        }
-
-        config = mergeConfig(this.defaults, config)// 混合默认配置与自定义配置
-
-        const chain: PromiseChain<any>[] = [{// 链式调用  这里用any泛型来代替 AxiosPromise或者AxiosRequestConfig
-            resolved: dispatchRequest,
-            rejected: undefined
-        }]
-        this.interceptors.request.forEach(interceptor => {
-            chain.unshift(interceptor)// 请求拦截器 后添加的先执行
-        })
-        this.interceptors.response.forEach(interceptor => {
-            chain.push(interceptor) // 响应拦截器 先添加的先执行
-        })
-
-        let promise = Promise.resolve(config)
-
-        while (chain.length) {
-            const { resolved, rejected } = chain.shift()! // 这里断言不为空
-            promise = promise.then(resolved, rejected)
-        }
-
-        return promise
-        // return dispatchRequest(config)
+  }
+  // request(config: AxiosRequestConfig): AxiosPromise {
+  //     return dispatchRequest(config)
+  // }
+  // 函数重载扩展请求
+  request(url: any, config?: any): AxiosPromise {
+    if (typeof url === 'string') {
+      // 对应 axios('url',config)
+      if (!config) config = {}
+      config.url = url
+    } else {
+      config = url // 对应 axios(config)
     }
 
-    get(url: string, config?: AxiosRequestConfig): AxiosPromise {
-        return this._requestMethodWithoutData('get', url, config)
+    config = mergeConfig(this.defaults, config) // 混合默认配置与自定义配置
+    config.method = config.method.toLowerCase() // 转小写
+
+    const chain: PromiseChain<any>[] = [
+      {
+        // 链式调用  这里用any泛型来代替 AxiosPromise或者AxiosRequestConfig
+        resolved: dispatchRequest,
+        rejected: undefined
+      }
+    ]
+    this.interceptors.request.forEach(interceptor => {
+      chain.unshift(interceptor) // 请求拦截器 后添加的先执行
+    })
+    this.interceptors.response.forEach(interceptor => {
+      chain.push(interceptor) // 响应拦截器 先添加的先执行
+    })
+
+    let promise = Promise.resolve(config)
+
+    while (chain.length) {
+      const { resolved, rejected } = chain.shift()! // 这里断言不为空
+      promise = promise.then(resolved, rejected)
     }
 
-    delete(url: string, config?: AxiosRequestConfig): AxiosPromise {
-        return this._requestMethodWithoutData('delete', url, config)
-    }
+    return promise
+    // return dispatchRequest(config)
+  }
 
-    head(url: string, config?: AxiosRequestConfig): AxiosPromise {
-        return this._requestMethodWithoutData('head', url, config)
-    }
+  get(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithoutData('get', url, config)
+  }
 
-    options(url: string, config?: AxiosRequestConfig): AxiosPromise {
-        return this._requestMethodWithoutData('options', url, config)
-    }
+  delete(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithoutData('delete', url, config)
+  }
 
-    post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
-        return this._requestMethodWithData('post', url, data, config)
-    }
+  head(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithoutData('head', url, config)
+  }
 
-    put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
-        return this._requestMethodWithData('put', url, data, config)
-    }
+  options(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithoutData('options', url, config)
+  }
 
-    patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
-        return this._requestMethodWithData('patch', url, data, config)
-    }
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithData('post', url, data, config)
+  }
 
-    getUri(config?: AxiosRequestConfig): string {
-        config = mergeConfig(this.defaults, config)
-        return transformURL(config)
-    }
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithData('put', url, data, config)
+  }
 
-    _requestMethodWithoutData(method: Method, url: string, config?: AxiosRequestConfig): AxiosPromise {
-        return this.request(Object.assign(config || {}, {
-            method,
-            url
-        }))
-    }
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithData('patch', url, data, config)
+  }
 
-    _requestMethodWithData(method: Method, url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
-        return this.request(Object.assign(config || {}, {
-            method,
-            url,
-            data
-        }))
-    }
+  getUri(config?: AxiosRequestConfig): string {
+    config = mergeConfig(this.defaults, config)
+    return transformURL(config)
+  }
+
+  _requestMethodWithoutData(
+    method: Method,
+    url: string,
+    config?: AxiosRequestConfig
+  ): AxiosPromise {
+    return this.request(
+      Object.assign(config || {}, {
+        method,
+        url
+      })
+    )
+  }
+
+  _requestMethodWithData(
+    method: Method,
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): AxiosPromise {
+    return this.request(
+      Object.assign(config || {}, {
+        method,
+        url,
+        data
+      })
+    )
+  }
 }
--- src/core/dispatchRequest.ts
+++ src/core/dispatchRequest.ts
@@ -7,26 +7,26 @@ import { buildURL, isAbsoluteURL, combineURL } from '../helpers/url'
 // import { processHeaders, flattenHeaders } from '../helpers/headers'
 import { flattenHeaders } from '../helpers/headers'
 
-import transform from "./transform"
+import transform from './transform'
 
 import xhr from './xhr'
-
+import { error } from 'console'
 
 const processConfig: (config: AxiosRequestConfig) => void = config => {
-    config.url = transformURL(config)
-    // config.headers = transformHeaders(config)
-    // config.data = transformRequestData(config)
-    config.data = transform(config.data, config.headers, config.transformRequest)
-    config.headers = flattenHeaders(config.headers, config.method!) // 采取断言，这里的method在运行时一定有
+  config.url = transformURL(config)
+  // config.headers = transformHeaders(config)
+  // config.data = transformRequestData(config)
+  config.data = transform(config.data, config.headers, config.transformRequest)
+  config.headers = flattenHeaders(config.headers, config.method!) // 采取断言，这里的method在运行时一定有
 }
 
 // 转换url
 export const transformURL: (config: AxiosRequestConfig) => string = config => {
-    let { url, params, paramsSerializer, baseURL } = config
-    if (baseURL && !isAbsoluteURL(url!)) {
-        url = combineURL(baseURL, url)
-    }
-    return buildURL(url!, params, paramsSerializer) // 这里采取类型断言，断言这个url不会为空
+  let { url, params, paramsSerializer, baseURL } = config
+  if (baseURL && !isAbsoluteURL(url!)) {
+    url = combineURL(baseURL, url)
+  }
+  return buildURL(url!, params, paramsSerializer) // 这里采取类型断言，断言这个url不会为空
 }
 
 // // 转换请求的data
@@ -42,21 +42,31 @@ export const transformURL: (config: AxiosRequestConfig) => string = config => {
 // }
 
 const transformResponseData: (res: AxiosResponse) => AxiosResponse = res => {
-    // res.data = transformResponse(res.data)
-    res.data = transform(res.data, res.headers, res.config.transformResponse)
-    return res
+  // res.data = transformResponse(res.data)
+  res.data = transform(res.data, res.headers, res.config.transformResponse)
+  return res
 }
 
+const throwIfCancellationRequest: (config: AxiosRequestConfig) => void = config => {
+  if (config.cancelToken) {
+    // 这里表示token使用过了
+    config.cancelToken.throwIfRequested()
+  }
+}
 
-const throwIfCancellationRequest: (config: AxiosRequestConfig) => void = (config) => {
-    if (config.cancelToken) {// 这里表示token使用过了
-        config.cancelToken.throwIfRequested()
+const dispatchRequest: (config: AxiosRequestConfig) => AxiosPromise = config => {
+  throwIfCancellationRequest(config) // 校验是否使用过了token
+  processConfig(config)
+  return xhr(config).then(
+    res => {
+      return transformResponseData(res)
+    },
+    error => {
+      if (error && error.response) {
+        error.response = transformResponseData(error.response)
+      }
+      return Promise.reject(error)
     }
-}
-
-const dispatchRequest: (config: AxiosRequestConfig) => AxiosPromise = (config) => {
-    throwIfCancellationRequest(config) // 校验是否使用过了token
-    processConfig(config)
-    return xhr(config).then(res => transformResponseData(res))
+  )
 }
 export default dispatchRequest
